8a0965059bbc7d5958fb5cd5e3b4a1410e815160
WX-SORCE <2026089323@qq.com>
2025-04-03 12:03:43 +0800

    Delete portfolio-service directory

--- portfolio-service/src/main/java/com/alxy/portfolioservice/service/ProductValueService.java
+++
@@ -1,9 +0,0 @@
-package com.alxy.portfolioservice.service;
-
-import com.alxy.portfolioservice.entiy.ProductValueHistory;
-
-import java.util.List;
-
-public interface ProductValueService {
-    List<ProductValueHistory> getValueHistory(String itemId);
-}
